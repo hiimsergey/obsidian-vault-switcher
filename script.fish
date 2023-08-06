f24a7f76f1ac7c111cc74ab39f43bc7d3646611c
serg <91432388+hiimsergey@users.noreply.github.com>
2023-08-06 20:59:50 +0200

    Create script.fish

---
+++ script.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/fish
+
+while true
+    set select (echo -e "vault_one\nvault_two\nvault_three\nquit" | rofi -dmenu -p "Select vault:")
+
+    switch $select
+    case "vault_one" "vault_two" "vault_three"
+        cat ~/.config/obsidian/$select.json > ~/.config/obsidian/obsidian.json
+        break
+    case "quit"
+        exit
+    case "*"
+        continue
+    end
+end
+
+         # If you happen to be using Wayland
+obsidian #--enable-features=UseOzonePlatform --ozone-platform=wayland
