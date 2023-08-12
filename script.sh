59f4d4acce78ab65bc0fe7222c72f67e78bbb87c
serg <91432388+hiimsergey@users.noreply.github.com>
2023-08-12 15:08:59 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 while true; do
-    select=$(echo -e "vault_one\nvault_two\nvault_three\nquit" | rofi -dmenu -p "Select vault:")
+    select=$(echo -e "vault_one\nvault_two\nvault_three" | rofi -dmenu -p "Select vault:")
+
+    if [ -z "$select" ]; then
+        exit 1
+    fi
 
     case "$select" in
         "vault_one" | "vault_two" | "vault_three")
             cat ~/.config/obsidian/$select.json > ~/.config/obsidian/obsidian.json
             break
-            ;;
-        "quit")
-            exit
             ;;
         *)
             continue
